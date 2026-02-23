06663bbefd815535ab32d1c89c600763c5d89f09
Murtada saleh <83772089+wadsleh@users.noreply.github.com>
2026-02-23 13:56:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); 
 const auth = getAuth(app); 
 const googleProvider = new GoogleAuthProvider();
-const 
+const key = gitignore.env.
 window.currentUser = ""; 
 window.selectedNetwork = ""; 
 window.currentService = "";
