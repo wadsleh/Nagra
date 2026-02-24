b6eba5797b847abda346c68beb0032b973bd5785
Murtada saleh <83772089+wadsleh@users.noreply.github.com>
2026-02-24 14:32:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ window.openOrderModal = (s) => {
                 <p>ببجي</p>
             </div>
             <div class="network-card" onclick="proceedToStep2('Free Fire')">
-                <div class="pro-logo"><img src="images/freefire.png.jpg" alt="فري فاير"></div>
+                <div class="pro-logo"><img src="images/freefire.jpg" alt="فري فاير"></div>
                 <p>فري فاير</p>
             </div>
         `;
